b9c34e9d260b790d8d873594a93f4ca3a0d57829
Kashika Jindal <kashika249@gmail.com>
2025-06-23 16:45:14 +0530

    fixed store import

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { store } from './store.js'
-import { Provider } from 'react-redux'
-import App from './App.jsx'
-import { Toaster } from 'react-hot-toast'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <App />
       <Toaster />
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
